3c3d3fc23744f97397825cde0048715836f2604c
Pietro Di Bello <pierodibello@gmail.com>
2021-10-08 15:16:05 +0200

    rename variable

--- src/test/java/ReserveSeatsForScreeningTest.java
+++ src/test/java/ReserveSeatsForScreeningTest.java
@@ -12,14 +12,10 @@ public class ReserveSeatsForScreeningTest {
     void customer_reserves_seats_for_screening_starting_at_specific_time_when_available_gets_status_ok() {
         LocalDateTime screeningTime = LocalDateTime.now();
         List<Integer> seats = Arrays.asList(42, 45, 89);
+        Screenings screenings = scheduleTime -> new Screening();
 
         ReserveSeatsForScreeningCommand command = new ReserveSeatsForScreeningCommand(seats, screeningTime);
 
-        Screenings screenings = new Screenings() {
-            @Override public Screening byTime(LocalDateTime time) {
-                return new Screening();
-            }
-        };
         Boolean reserved = new SeatsReservationCommandHandler(screenings).handle(command);
 
         assertThat(reserved).isTrue();
