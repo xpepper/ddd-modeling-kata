ddbc25c5dcb00cb41523b1a5b49b61ff07ed8ff8
Pietro Di Bello <pierodibello@gmail.com>
2021-10-12 23:06:12 +0200

    add one more test

--- src/test/java/ScreeningTest.java
+++ src/test/java/ScreeningTest.java
@@ -23,6 +23,14 @@ public class ScreeningTest {
     }
 
     @Test
+    void does_not_reserve_seat_when_at_least_one_is_unavailable() {
+        Screening screening = new Screening(Seat.available(1), Seat.available(2), Seat.reserved(3));
+        Boolean reserved = screening.reserveSeats(asList(1, 3));
+
+        assertThat(reserved).isFalse();
+    }
+
+    @Test
     void cannot_reserve_twice_the_same_seats() {
         Screening screening = new Screening(Seat.available(1), Seat.reserved(2));
         assertTrue(screening.reserveSeats(asList(1)));
