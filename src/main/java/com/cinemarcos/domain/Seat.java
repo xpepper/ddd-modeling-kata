41bf2830bcaed2bea5723d52a071a4c639c5dfe7
Pietro Di Bello <pierodibello@gmail.com>
2021-10-14 23:28:00 +0200

    fix Seat#toString

--- src/main/java/com/cinemarcos/domain/Seat.java
+++ src/main/java/com/cinemarcos/domain/Seat.java
@@ -3,6 +3,7 @@ package com.cinemarcos.domain;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class Seat {
     final int seatNumber;
@@ -43,9 +44,6 @@ public class Seat {
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this)
-                .append("seatNumber", seatNumber)
-                .append("isReserved", isAvailable)
-                .toString();
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
     }
 }
