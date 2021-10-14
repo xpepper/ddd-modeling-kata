b3a2c4ddb329a87b0a50592c8f430961eeac19b9
Pietro Di Bello <pierodibello@gmail.com>
2021-10-14 16:23:27 +0200

    add TODO

--- src/main/java/ReserveSeatsCommandHandler.java
+++ src/main/java/ReserveSeatsCommandHandler.java
@@ -6,7 +6,7 @@ public class ReserveSeatsCommandHandler {
     }
 
     public Boolean handle(ReserveSeatsCommand command) {
-        Screening screening = screenings.byTime(command.time); //TODO aggiungere anche il time allo screening
+        Screening screening = screenings.byTime(command.time);
         Boolean seatsReserved = screening.reserveSeats(command.seats);
         if (seatsReserved)
             screenings.save(screening);
@@ -14,3 +14,13 @@ public class ReserveSeatsCommandHandler {
         return seatsReserved;
     }
 }
+/*
+TODO:
+    * add an ID to the Screening aggregate to be able to retrive only its events
+    * introduce an event store (as a dependency to the command handler) to read the events to pass to Screening.from(event) (we have to get the events from somewhere)
+    * Screening#researveSeats has to fire a ScreeningSeatsReserved (e.g. `eventstore.publish(event)`)
+    * Event store has to store the events
+    * add a scheduleTime to Screening (we forgot to add it before...)
+
+*/
+
